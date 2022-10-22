4a79b1de11b3440d91b62e0d43b16c2455f9fef4
Ariana Ivan <hi@arianaivan.com>
2022-10-22 00:07:16 -0700

    Initial set-up

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,mdx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
