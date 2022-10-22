5a4a503d876c8cd4eb912c0f90f0cd2c8d8ebbb4
Ariana Ivan <hi@arianaivan.com>
2022-10-22 10:59:52 -0700

    Write instruction of what i want to do, but i am blocked

---
+++ pages/art/ArtGrid.tsx
@@ -0,0 +1,1 @@
+// i want to write the code of the artgrid here in this file, and then to invoke it as a function and appear in the index.mdx; so that i can keep the index.mdx (home page) clean and simple
