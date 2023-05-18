0eec1ab59c883129ee1da2731b69d6973a39f62c
Ariana Ivan <git@arianaivan.com>
2023-05-18 16:32:30 -0700

    Getting & displaying the images from art-images repo

--- src/ArtGrid.jsx
+++ src/ArtGrid.jsx
@@ -1,11 +1,5 @@
 import Image from 'next/image'
 
-const imageFiles = [
-  '/images/testing1.jpeg',
-  '/images/testing2.jpeg',
-  '/images/testing3.jpeg'
-]
-
 export function ArtGrid({ images }) {
   return (
     <div>
@@ -14,7 +8,8 @@ export function ArtGrid({ images }) {
         ?.slice()
         .reverse()
         .map((image, index) => (
-          <Image
+          <img
+            className="inline"
             key={index}
             src={image}
             alt="testing"
--- src/ArtLoader.jsx
+++ src/ArtLoader.jsx
@@ -1,15 +1,24 @@
-import image1 from '../images/testing1.jpeg'
-import image2 from '../images/testing2.jpeg'
-import image3 from '../images/testing3.jpeg'
-import image4 from '../images/testing4.jpeg'
+import { useEffect, useState } from 'react'
 import { ArtGrid } from '../src/ArtGrid'
 
-const images = [image1, image2, image3, image4]
-
 export function ArtLoader() {
-  return (
-    <>
-      <ArtGrid images={images} />
-    </>
-  )
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+    fetch('https://arianaivan.github.io/art-images/all-images.json')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setImages(
+          data.map(
+            (item) => 'https://arianaivan.github.io/art-images' + item.file
+          )
+        )
+      })
+      .catch((error) => {
+        console.error('Error fetching image data:', error)
+      })
+  }, [])
+
+  return <ArtGrid images={images} />
 }
