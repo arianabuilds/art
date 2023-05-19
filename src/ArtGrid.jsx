6f8821584ad3f58602478b0139d0507447dc5fe5
Ariana Ivan <git@arianaivan.com>
2023-05-18 21:14:32 -0700

    Started adding Modal to ArtGrid

--- src/ArtGrid.jsx
+++ src/ArtGrid.jsx
@@ -13,6 +13,9 @@ export function ArtGrid({ images }) {
             width={100}
           />
         ))}
+      <div className="fixed top-0 left-0 z-50 flex items-center justify-center h-full bg-black bg-opacity-90 ">
+        <img className="w-2/3" src={images[0]} />
+      </div>
     </div>
   )
 }
