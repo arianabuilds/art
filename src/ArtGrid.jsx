04ae8884113f2a6f8aff4544a189d89f24bd5921
Ariana Ivan <git@arianaivan.com>
2023-05-18 18:06:39 -0700

    made the grid look better

--- src/ArtGrid.jsx
+++ src/ArtGrid.jsx
@@ -1,29 +1,18 @@
-import Image from 'next/image'
-
 export function ArtGrid({ images }) {
   return (
-    <div>
+    <div className="relative">
       <h2>ArtGrid Component</h2>
       {images
         ?.slice()
         .reverse()
         .map((image, index) => (
           <img
-            className="inline"
+            className="inline object-cover !my-0 aspect-square hover:aspect-auto hover:scale-[350%] transition"
             key={index}
             src={image}
-            alt="testing"
             width={100}
-            height={100}
           />
         ))}
     </div>
   )
 }
-
-// - [x] Load a list of images
-// - [x] All images will be a small square
-// - [ ] Display the images in a grid
-// - [ ] Order the grid from:
-//         top left: newest
-//         bottom right: oldest
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: [
     './pages/**/*.{js,jsx,ts,tsx,mdx}',
-    './components/**/*.{js,jsx,ts,tsx,mdx}'
+    './src/**/*.{js,jsx,ts,tsx,mdx}'
   ],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [],
+  future: {
+    hoverOnlyWhenSupported: true
+  }
 }
