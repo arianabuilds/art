feb4cab91bab1bd9c80b6b3609bdb31c11d807d3
Ariana Ivan <git@arianaivan.com>
2023-05-20 12:14:33 -0700

    ArtLoader: trying to implement code for loading images 10% at a time

--- src/ArtLoader.jsx
+++ src/ArtLoader.jsx
@@ -3,17 +3,14 @@ import { ArtGrid } from '../src/ArtGrid'
 
 export function ArtLoader() {
   const [images, setImages] = useState([])
+  const [loadCount, setLoadCount] = useState(1) // Added this state to keep track of loading phase
 
   useEffect(() => {
     fetch('https://arianaivan.github.io/art-images/all-images.json')
       .then((response) => response.json())
       .then((data) => {
-        // Convert data to include date objects for sorting
         const processedData = data.map((item) => {
-          // Check if the file name can be converted to a date
-          const dateString = item.file.split('/').pop().split('.')[0] // Assumes date is at the end of the file name
-
-          // Convert the first three hyphenated sections to a date
+          const dateString = item.file.split('/').pop().split('.')[0]
           const date = new Date(dateString.split('-').slice(0, 3).join('-'))
 
           return {
@@ -23,12 +20,10 @@ export function ArtLoader() {
           }
         })
 
-        // Sort the processed data by date, then by file name (which includes the index for multiple files per day)
         processedData.sort(
           (b, a) => a.date - b.date || a.file.localeCompare(b.file)
         )
 
-        // Set the sorted image URLs
         setImages(processedData)
       })
       .catch((error) => {
@@ -36,5 +31,11 @@ export function ArtLoader() {
       })
   }, [])
 
-  return <ArtGrid images={images} />
+  return (
+    <ArtGrid
+      images={images}
+      loadCount={loadCount}
+      incrementLoadCount={() => setLoadCount(loadCount + 1)}
+    />
+  )
 }
